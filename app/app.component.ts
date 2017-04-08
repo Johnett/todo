31253244bc2a85aa3151df358f791fe26ae7f55b
Johnett Mathew <johnettwst@gmail.com>
2017-04-08 23:39:41 +0530

    Todo App Complete Files Updated

---
+++ app/app.component.ts
@@ -0,0 +1,29 @@
+import {Component} from "@angular/core";
+import {Model, TodoItem} from "./model";
+
+@Component({
+    selector:"todo-app",
+    templateUrl:"app/app.component.html"
+})
+
+export class AppComponent{
+    model =new Model();
+
+    getName()
+    {
+        return this.model.user;
+    }
+    getTodoItems()
+    {
+        return this.model.items.filter(function(items){
+            return !items.done;
+        })
+    }
+    addItem(newItem)
+    {
+        if(newItem !="")
+        {
+            this.model.items.push(new TodoItem(newItem ,false));
+        }
+    }
+}
